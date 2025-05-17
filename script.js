58476af768756b838218b0928351287486db6806
fernajapb <fernaaguilarpau@gmail.com>
2025-05-17 15:18:10 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-      document.getElementById('menu').classList.toggle('show');
-    }
-
-    function toggleSubMenu(element) {
-      if (window.innerWidth <= 768) {
-        element.classList.toggle('open');
-      }
-    }
