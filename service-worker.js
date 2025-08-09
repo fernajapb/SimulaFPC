c4aa9dde395c099a873994fa822abdf1b1437e86
fernajapb <fernaaguilarpau@gmail.com>
2025-08-09 00:16:53 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "simulafpc-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+// Instalar y guardar en caché
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activar y limpiar cachés viejas
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+});
+
+// Interceptar peticiones
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
