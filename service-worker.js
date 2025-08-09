0f2026393ac51e78f9ff8aea30d41ffce31294c9
fernajapb <fernaaguilarpau@gmail.com>
2025-08-09 00:42:45 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "simulafpc-v1";
+const urlsToCache = ["/SimulaFPC/", "/SimulaFPC/index.html"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
